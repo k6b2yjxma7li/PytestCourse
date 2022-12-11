8c0ff8d8e39d724c1b63df3388aa12c35e8589e0
k6b2yjxma7li <rkrasala@gmail.com>
2022-12-11 22:11:23 +0100

    Basic backend for companies view

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Company
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from .serializers import CompanySerializer
 
-# Create your views here.
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
+    pagination_class = PageNumberPagination
